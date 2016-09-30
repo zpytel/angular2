ed6786d45bfccf5eaf8d7eb8bb265fd60fb8365b
zpytel <zbigniew.pytel@gmail.com>
2016-09-30 14:33:40 -0400

    changes by Stive
    added message to home

--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core';
 
 @Component({
-    template:`<h1>I'm in home</h1>`
+    template:`<h1>I'm in home</h1> this is from stuff`
 })
 export class HomeComponent{}
